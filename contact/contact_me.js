4bed6d1162f45e92b156f2aa4289d02b9b5d96ba
Miguel camacho <miguelacs18@gmail.com>
2019-05-30 12:43:04 -0400

    form

--- contact/contact_me.js
+++ contact/contact_me.js
@@ -1,68 +1,24 @@
-$(function() {
-
-    $("input,textarea").jqBootstrapValidation({
-        preventSubmit: true,
-        submitError: function($form, event, errors) {
-            // additional error messages or events
+var form = $('#form'); //form id here
+form.submit(function(event) {
+    event.preventDefault();
+    var form_status = $('<div class="form_status"></div>');
+    var $form = $(this);
+    $.ajax({
+        type: 'POST',
+        url: "sendemail.php",
+        data: {
+            Name: $("#name").val(),
+            Email: $("#email").val(),
+            Message: $("#message").val()
         },
-        submitSuccess: function($form, event) {
-            event.preventDefault(); // prevent default submit behaviour
-            // get values from FORM
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var message = $("textarea#message").val();
-            var firstName = name; // For Success/Failure Message
-            // Check for white space in name for Success/Fail message
-            if (firstName.indexOf(' ') >= 0) {
-                firstName = name.split(' ').slice(0, -1).join(' ');
-            }
-            $.ajax({
-                url: "contact_me.php",
-                type: "POST",
-                data: {
-                    name: name,
-                    email: email,
-                    message: message
-                },
-                cache: false,
-                success: function() {
-                    // Success message
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
-
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-                error: function() {
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-                    $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-            })
-        },
-        filter: function() {
-            return $(this).is(":visible");
-        },
+        beforeSend: function() {
+            form.append(form_status.html('Email is sending...').fadeIn());
+        }
+    }).done(function(data) {
+        form_status.html('Email Sent!').delay(3210).fadeOut();
     });
-
-    $("a[data-toggle=\"tab\"]").click(function(e) {
-        e.preventDefault();
-        $(this).tab("show");
-    });
-});
-
-
-/*When clicking on Full hide fail/success boxes */
-$('#name').focus(function() {
-    $('#success').html('');
+    //delete messages when submit
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
 });
--- contact/contact_me.php
+++ contact/contact_me.php
@@ -1,24 +1,17 @@
-<?php
-// check if fields passed are empty
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-	echo "No arguments Provided!";
-	return false;
-   }
-	
-$name = $_POST['name'];
-$email_address = $_POST['email'];
-$message = $_POST['message'];
-	
-// create email body and send it	
-$to = 'miguelacs18@gmail.com'; // ****PUT YOUR EMAIL ADDRESS HERE****
-$email_subject = "Contacto Miguel Portafolio:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: CONTACT-FORM@YOUR-WEBSITE.com\n"; // ****ENTER WHO YOU WANT THE MESSAGE TO BE FROM HERE****
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
-return true;			
-?>
+    <?php
+    ini_set("mail.log", "/tmp/mail.log");
+    ini_set("mail.add_x_header", TRUE);
+    //variables
+    $name = strip_tags(htmlspecialchars($_POST['Name']));
+    $email_address = strip_tags(htmlspecialchars($_POST['Email']));
+    $message = strip_tags(htmlspecialchars($_POST['Message']));
+       
+    // Create the email and send the message
+    $to = 'miguelacs18@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+    $email_subject = "Website Contact Form:  $name";
+    $email_body = "You have received a new message from example... contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
+    $headers = "From: noreply@tsigaradiko.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+    $headers .= "Reply-To: $email_address"; 
+    if(mail($to,$email_subject,$email_body,$headers)){ echo "Mail sent!";} else{ echo "Error, check your logs."; }
+    return true;
+    ?>
